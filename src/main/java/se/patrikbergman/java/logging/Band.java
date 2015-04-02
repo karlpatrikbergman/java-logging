0cce433af1c103cb52a8e907f036bb4358d005a3
karlpatrikbergman <patrik_bergman@yahoo.com>
2015-04-02 11:14:31 +0200

    last day at work

---
+++ src/main/java/se/patrikbergman/java/logging/Band.java
@@ -0,0 +1,19 @@
+package se.patrikbergman.java.logging;
+
+public class Band {
+	private final String name;
+	private final String descriptions;
+
+	public Band(String name, String descriptions) {
+		this.name = name;
+		this.descriptions = descriptions;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescriptions() {
+		return descriptions;
+	}
+}
